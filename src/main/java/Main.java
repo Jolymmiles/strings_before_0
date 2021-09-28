aa7e39a0daa2afddb11007afaa35d14ac018eee6
Валентин <valmak04@gmail.com>
2021-09-28 19:28:39 +0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+        int i = 0;
+        String in_string1;
+        do {
+            in_string1 = sc.nextLine();
+            words.add(in_string1);
+            i += 1;
+        } while (in_string1.compareTo("0") != 0);
+        for (int count = 0; count < i -1; count += 1) System.out.println(words.get(count));
+        }
     }
-}
